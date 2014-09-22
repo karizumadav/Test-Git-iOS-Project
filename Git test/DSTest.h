e6e866ffd89310e150b2c42039d1d0454f04a43e
Davion Scott <karizumatikku@aol.com>
2014-09-22 19:59:52 -0400

    added a test class

---
+++ Git test/DSTest.h
@@ -0,0 +1,13 @@
+//
+//  DSTest.h
+//  Git test
+//
+//  Created by Davion Scott on 9/22/14.
+//  Copyright (c) 2014 Scott Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSTest : NSObject
+
+@end
